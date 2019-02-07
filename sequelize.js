6dbe02be7d826160cfdc70a683a17442bf58ad89
Luiz Augusto <luiz.moraes@drtis.com.br>
2019-02-07 00:54:10 -0200

    alterando configuração de acesso ao banco utilizando docker

--- sequelize.js
+++ sequelize.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize({
-  username: process.env.DB_USERNAME,
+  username: 'postgres',
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  logging: process.env.DB_LOGGING ? process.env.DB_LOGGING === 'true' : false,
+  database: 'books-db',
+  host: 'books-db',
+  port: 5432,
+  logging: false,
   dialect: 'postgres',
   migrationStorage: 'sequelize',
   seederStorage: 'sequelize',
